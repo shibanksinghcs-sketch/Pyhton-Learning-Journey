330ba85584dda28fc6d25ec509e5f41a2c898a39
Shivank <Shibanksingh.cs@gmail.com>
2026-03-15 12:17:10 +0530

    Add Week-2
    Programs:
    - A Multiplication Table Generator  Using Loop.

--- Week-2/Multiplication_Table_Generator.py
+++ Week-2/Multiplication_Table_Generator.py
@@ -5,10 +5,11 @@ print("\t\tLets Test It!!")
 #Taking A Number As Input From User and Storing In Variables Named as "Num_1".
 Num_1=int(input("\nEnter a Number: "))
 i=1
+
 #Main Logic Using While loop.
 while i<=10:
     print(f"{Num_1} * {i} = {Num_1*i}")
     i=i+1
 
-#Program End
+#Program End.
 print("\t\n\t\tThankyou!!")
